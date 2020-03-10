7ca94b9f584c74116a2a42d32a3c8f8c20c8aa80
Halex Viotto Gomes <halexx002@gmail.com>
2020-03-09 23:09:10 -0400

    Tic-tac-toe game completed with additional features listed in the index.js file as Challenge TODO and Custom Challenge TODO.

--- React/tutorial-app/src/index.js
+++ React/tutorial-app/src/index.js
@@ -4,31 +4,98 @@ import './index.css';
 
 
 function Square(props) {
+       
+    if (props.statusTile === 1) {
+      return (
+        <button 
+          className="square winnerTiles" 
+          onClick={props.onClick}
+        >
+          {props.value}
+        </button>
+      );
+    } else if (props.statusTile === 2) {
+      return (
+        <button 
+          className="square highlightMove" 
+          onClick={props.onClick}
+        >
+          {props.value}
+        </button>
+      );
+    } else {
+      return (
+        <button 
+          className="square" 
+          onClick={props.onClick}
+        >
+          {props.value}
+        </button>
+      );
+    }
+
     
-    return (
-      <button 
-        className="square" 
-        onClick={props.onClick}
-      >
-        {props.value}
-      </button>
-    );
     
   }
   
   class Board extends React.Component {
       
-    renderSquare(i) {
+    renderSquare(i, statusTile) {
       return (
        <Square 
          value={this.props.squares[i]}
          onClick={() => this.props.onClick(i)}
+         statusTile={statusTile}
        />
       );
     }
+    
+    //It builds the board with two for loops.
+    renderRowsAndCols(statusTiles) {
+      let counter = 0;//It is used as a parameter to the renderSquare method.
+      
+      let rows = [];
+      let cols;
+      let statTiles = [0,0,0,0,0,0,0,0,0];
+
+      if (statusTiles.length === 3) {
+        for (let index = 0; index < statusTiles.length; index++) {
+          statTiles[statusTiles[index]] = 1;
+        }
+      } else {
+        if (statusTiles.length === 1) {
+          for (let index = 0; index < statusTiles.length; index++) {
+            statTiles[statusTiles[index]] = 2;
+          }
+        } 
+      }
+
+      
+
+      for (let i = 0; i < 3; i++) {
+        cols = [];
+        for (let j = 0; j < 3; j++) {
+          cols.push(this.renderSquare(counter, statTiles[counter]));
+          counter++;
+        }
+
+      rows.push(<div className="board-row">{cols}</div>);
+
+      }
+
+      return (rows);
+
+    }
+    
+    
   
     render() {
+      
       return (
+        <div>{this.renderRowsAndCols(this.props.statusTiles)}</div>
+      );
+
+      /*return (//Old version to construct the board.
         <div>
           <div className="board-row">
             {this.renderSquare(0)}
@@ -46,7 +113,7 @@ function Square(props) {
             {this.renderSquare(8)}
           </div>
         </div>
-      );
+      );*/
     }
   }
   
@@ -56,26 +123,68 @@ function Square(props) {
       this.state = {
         history: [{
           squares: Array(9).fill(null),
+          position: [null,null], //To save the position X,Y where it was marked.
         }],
         stepNumber: 0,
         xIsNext: true,
+        isAscending: true,//A flag for the type of sorting. Ascending or Descending.
       };
     }
 
     handleClick(i){
+
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
 
       const current = history[history.length - 1];
 
       const squares = current.squares.slice();
       
-      if (calculateWinner(squares) || squares[i]) {
+      if (calculateWinner(squares)[0] || squares[i]) {
         return;
       }
+
+      let whereClicked; //Hold the actual position where It was clicked on the board.
+      
+      switch (i) { //It Map the coordinates of the board.
+        case 0:
+          whereClicked = [1,1];
+          break;
+        case 1:
+          whereClicked = [1,2];
+          break;
+        case 2:
+          whereClicked = [1,3];
+          break;
+        case 3:
+          whereClicked = [2,1];
+          break;
+        case 4:
+          whereClicked = [2,2];
+          break;
+        case 5:
+          whereClicked = [2,3];
+          break;
+        case 6:
+          whereClicked = [3,1];
+          break;
+        case 7:
+          whereClicked = [3,2];
+          break;
+        case 8:
+          whereClicked = [3,3];
+          break;          
+        default:
+          console.log("Error where It was clicked.")
+          break;
+      }
+
+      whereClicked.push(i);
+
       squares[i] = this.state.xIsNext ? 'X' : 'O';
       this.setState({
         history: history.concat([{
           squares: squares,
+          position: whereClicked,//It saving the coordinates.
         }]),
         stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
@@ -89,30 +198,63 @@ function Square(props) {
       });
     }
 
+    changeSorting () {//It changes the value of flag.
+      this.setState({
+        isAscending: !this.state.isAscending,
+      });
+    }
+
     render() {
       const history = this.state.history;
+
+      const currentStep = this.state.stepNumber;//Where it currently is on the history list.
 
       const current = history[this.state.stepNumber];
 
       const winner = calculateWinner(current.squares);
 
       const moves = history.map((step, move) => {
-        const desc = move ? 'Go to move #' + move : 'Go to game start';
+        const desc = move ? 'Go to move #' + move + ' (' + step.position[0] + ',' + step.position[1] + ')' : 'Go to game start';
 
-        return (
-          <li key={move}>
-            <button onClick={() => this.jumpTo(move)}>{desc}</button>
-          </li>
-        );
-
+        if (currentStep === move) {//Where on the list it is.
+          return (//Where in the history the match is.
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}><b>{desc}</b></button>
+            </li>
+          );
+        } else {
+          return (
+            <li key={move}>
+              <button onClick={() => this.jumpTo(move)}>{desc}</button>
+            </li>
+          );
+        }
       })
+      
+      //This block change the sorting of the moves array from ascending to descending and vice-versa.
+      let sortButton;
+      if (this.state.isAscending) {
+        sortButton = (<button onClick={() => {this.changeSorting()}}>Sorted: Ascending</button>);
+        
+      } else {
+        sortButton = (<button onClick={() => {this.changeSorting()}}>Sorted: Descending</button>);
+
+        moves.reverse();
+      }
 
       let status;
 
-      if (winner) {
-        status = 'Winner: ' + winner;
+      if (winner[0]) {
+        status = 'Winner: ' + winner[0];
       } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+        if (this.state.stepNumber === 9) {
+          status = "It's a draw!";
+          winner[1] = [current.position[2]];
+        } else {
+          status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+          winner[1] = [current.position[2]];
+        }
+        
       }
 
       return (
@@ -121,19 +263,41 @@ function Square(props) {
             <Board 
               squares = {current.squares}
               onClick = {(i) => this.handleClick(i)}
+              statusTiles = {winner[1]}
             />
           </div>
           <div className="game-info">
             <div>{status}</div>
+            <div>{sortButton}</div>
             <ol>{moves}</ol>
           </div>
         </div>
       );
     }
   }
+
+  //background color: #50ec50;
   
   // ========================================
   
+  /*
+  ---Challenge TODO---
+  From: https://reactjs.org/tutorial/tutorial.html.
+  "If you have extra time or want to practice your new React skills, here are some ideas for improvements that you could make to the tic-tac-toe game which are listed in order of increasing difficulty:
+
+  - (COMPLETED) Display the location for each move in the format (col, row) in the move history list.
+  - (COMPLETED) Bold the currently selected item in the move list.
+  - (COMPLETED) Rewrite Board to use two loops to make the squares instead of hardcoding them.
+  - (COMPLETED) Add a toggle button that lets you sort the moves in either ascending or descending order.
+  - (COMPLETED) When someone wins, highlight the three squares that caused the win.
+  - (COMPLETED) When no one wins, display a message about the result being a draw."
+  */
+
+  /*
+  ---Custom Challenge TODO---
+  - (COMPLETED) Highlight where the move was made.
+  */
+
   ReactDOM.render(
     <Game />,
     document.getElementById('root')
@@ -153,8 +317,8 @@ function Square(props) {
     for (let i = 0; i < lines.length; i++) {
       const [a, b, c] = lines[i];
       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
+        return [squares[a],[a,b,c]];
       }
     }
-    return null;
+    return [null, []];
   }
